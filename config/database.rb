cf241955009346a731181590e9733b33b8763091
khadija khan <khadijakhan10@gmail.com>
2018-06-28 23:30:17 +0000

    Readies app for deployment

--- config/database.rb
+++ config/database.rb
@@ -7,7 +7,7 @@ configure do
     adapter: "sqlite3",
     database: "db/db.sqlite3"
   }
-else
+  else
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://eyeejnvscibmzd:264c06b48b2c256b078ae601e7c3804e6c95f0a85f595b05acac4a02b6620160@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d236pv5sd3lq68')
   set :database, {
   adapter: "postgresql",
@@ -17,12 +17,15 @@ else
   database: db.path[1..-1],
   encoding: "utf8"
   }
-
+  
+  end
+ 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
-
+  
 end
+
